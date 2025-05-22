56742b811b0b63c7b732c997d3dea5e693eacc61
English_Finn <englf105@hsd.k12.or.us>
2025-05-22 08:46:02 -0700

    saving unsaved changes

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,28 @@
 package com.example;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.TitledPane;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+
+    Button hireApplicant = new Button();
+
+    Button rejectApplicant = new Button();
+
+    TextArea requestedJobText = new TextArea();
+
+    TextArea applicantResumeText = new TextArea();
+
+    Label applicantName = new Label();
+    
+    TitledPane viewApplication = new TitledPane();
+
+    ImageView applicantImage = new ImageView();
+
 }
