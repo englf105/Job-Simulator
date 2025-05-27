8bc1126f03a4f8677a5721c00156e8f01fa06c5e
English_Finn <englf105@hsd.k12.or.us>
2025-05-27 09:51:02 -0700

    added UI functions and UI IDs

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -6,23 +6,30 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.text.TextFlow;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
     @FXML
-
     Button hireApplicant = new Button();
 
+    @FXML
     Button rejectApplicant = new Button();
 
+    @FXML
     TextArea requestedJobText = new TextArea();
 
+    @FXML
     TextArea applicantResumeText = new TextArea();
 
+    @FXML
     Label applicantName = new Label();
     
+    @FXML
     TitledPane viewApplication = new TitledPane();
 
+    @FXML
     ImageView applicantImage = new ImageView();
 
 }
