7baf96ea3e6a0e71e9b8f45a001b2796d3e0bd27
English_Finn <englf105@hsd.k12.or.us>
2025-05-29 09:48:22 -0700

    finished UI mockup

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 /**
@@ -17,6 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Job-Simulator");
         scene = new Scene(loadFXML("primaryController"), 640, 480);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,16 +1,28 @@
 package com.example;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
-public class PrimaryController {
 
+public class PrimaryController{
+
+    
     @FXML
     Button hireApplicant = new Button();
 
@@ -28,7 +40,7 @@ public class PrimaryController {
     
     @FXML
     TitledPane viewApplication = new TitledPane();
-
+    
     @FXML
     ImageView applicantImage = new ImageView();
 
